f37d10ca1e7acbc59d68f506a4574bbb668f6728
Polina Khmialnitskaya <polinagreenheart@gmail.com>
2023-09-24 13:57:53 +0300

    11-12

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,15 @@
 import React from 'react';
 import './App.css';
+import {Header} from "./components/Header";
+import {Navbar} from "./components/Navbar";
+import {Profile} from "./components/Profile";
 
 function App() {
     return (
         <div className="app-wrapper">
-            <header className={'header'}>
-                <img
-                    src={'https://static.vecteezy.com/system/resources/previews/022/841/114/original/chatgpt-logo-transparent-background-free-png.png'}/>
-            </header>
-            <nav className={'nav'}>
-                <div><a>Profile</a></div>
-                <div><a>Messages</a></div>
-                <div><a>News</a></div>
-                <div><a>Music</a></div>
-                <div><a>Settings</a></div>
-            </nav>
-            <div className={'content'}>
-                <img src={'https://www.fonedog.com/images/photo-compress/image-compressor-image.jpg'}/>
-                <div> ava + description</div>
-                <div> My posts
-                    <div>New post</div>
-                </div>
-                <div>
-                    <div>Post 1</div>
-                    <div>Post 2</div>
-                </div>
-
-            </div>
+           <Header/>
+            <Navbar/>
+           <Profile/>
         </div>
     );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <header className={'header'}>
+            <img
+                src={'https://static.vecteezy.com/system/resources/previews/022/841/114/original/chatgpt-logo-transparent-background-free-png.png'}/>
+        </header>
+    );
+};
+
