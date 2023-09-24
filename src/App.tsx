961f1b1aaeb31aa976ae8af955a43923c80aff7f
Polina Khmialnitskaya <polinagreenheart@gmail.com>
2023-09-24 13:15:06 +0300

    samurai

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import './App.css';
-import Technologies from "./Technologies";
-import Header from "./Header";
-import Footer from "./Footer";
 
 function App() {
     return (
-        <div className="App">
-            <Header/>
-            <Technologies/>
-            <Footer/>
+        <div className="app-wrapper">
+            <header className={'header'}>
+                <img
+                    src={'https://static.vecteezy.com/system/resources/previews/022/841/114/original/chatgpt-logo-transparent-background-free-png.png'}/>
+            </header>
+            <nav className={'nav'}>
+                <div><a>Profile</a></div>
+                <div><a>Messages</a></div>
+                <div><a>News</a></div>
+                <div><a>Music</a></div>
+                <div><a>Settings</a></div>
+            </nav>
+            <div className={'content'}>
+                <img src={'https://www.fonedog.com/images/photo-compress/image-compressor-image.jpg'}/>
+                <div> ava + description</div>
+                <div> My posts
+                    <div>New post</div>
+                </div>
+                <div>
+                    <div>Post 1</div>
+                    <div>Post 2</div>
+                </div>
+
+            </div>
         </div>
     );
 }
