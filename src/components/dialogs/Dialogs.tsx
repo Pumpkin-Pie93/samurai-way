aaebf5dd44dadfec3dc7a5e59d7da460b17fc50d
Polina Khmialnitskaya <polinagreenheart@gmail.com>
2023-10-27 15:11:57 +0300

    30-32

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useReducer, useRef} from 'react';
 import s from './Dialogs.module.css'
 import {NavLink} from "react-router-dom";
 import {DialogItem} from "./dialogItem/DialogItem";
@@ -6,20 +6,37 @@ import {Message} from "./message/Message";
 import {DialogsDataType, MessagesDataType} from "../../redux/State";
 
 export type DialogsPropsType = {
-    messages:MessagesDataType[]
+    messages: MessagesDataType[]
     dialogs: DialogsDataType[]
 }
 
 export const Dialogs = (props: DialogsPropsType) => {
-    const dialogs = props.dialogs.map( dialog => <DialogItem name={dialog.name} id={dialog.id} avatar={dialog.avatar}/>)
-    const messages = props.messages.map( message => <Message message={message.message}/>)
+
+    const dialogs = props.dialogs.map(dialog => <DialogItem name={dialog.name} id={dialog.id} avatar={dialog.avatar}/>)
+    const messages = props.messages.map(message => <Message message={message.message}/>)
+
+    let textAreaRef = useRef<HTMLTextAreaElement>(null)
+
+    const onClickHandler = () => {
+        if (textAreaRef.current !== null) {
+            alert(textAreaRef.current.value)
+        }
+    }
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
                 {dialogs}
             </div>
             <div className={s.messages}>
-                {messages}
+                <div>
+                    {messages}
+                </div>
+                <div className={s.textAreaAndButton}>
+                    <textarea ref={textAreaRef}/>
+                    <button onClick={onClickHandler}>add message</button>
+                </div>
+
             </div>
         </div>
 
--- src/components/profile/myPosts/MyPosts.tsx
+++ src/components/profile/myPosts/MyPosts.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import s from './MyPosts.module.css';
 import {Post} from "./post/Post";
 import {PostItemType} from "../../../redux/State";
@@ -14,16 +14,24 @@ export const MyPosts = (props: MyPostsPropsType) => {
 
     const posts = props.posts.map( (p, index)=> (<Post key={index} message={p.message} likes={p.likes}/>))
 
+    const newPost = useRef<HTMLTextAreaElement>(null)
+
+    const addNewPost = () => {
+    if(newPost.current !== null) {
+    alert(`Вы добавили пост: ${newPost.current.value}`)
+}
+    }
+
     return (
         <div>
             <div className={s.myPosts}>
                 <h3>My posts</h3>
                 <div >
                     <div>
-                        <textarea placeholder={'Enter your text here'}></textarea>
+                        <textarea ref={newPost} placeholder={'Enter your text here'}></textarea>
                     </div>
                     <div className={s.btnWrapper}>
-                        <button>add post</button>
+                        <button onClick={addNewPost}>add post</button>
                         <button>remove post</button>
                     </div>
                 </div>
