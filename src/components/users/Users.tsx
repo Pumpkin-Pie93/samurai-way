fce71ef864725a84534c495d91be36b47c4301d0
Polina Khmialnitskaya <polinagreenheart@gmail.com>
2024-02-07 20:58:44 +0300

    53

--- src/components/users/Users.tsx
+++ src/components/users/Users.tsx
@@ -4,47 +4,50 @@ import s from './Users.module.css'
 import axios from "axios";
 import userPhoto from '../../assets/images/ava.jpg'
 
-export const Users = (props: UsersPropsType) => {
-let getUsers = ()=> {
-    if(props.users.length === 0){
+export class Users extends React.Component<UsersPropsType> {
+    constructor(props:UsersPropsType) {
+        super(props);
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
-            .then((res:any)=>{
-                props.setUsers(res.data.items)
+            .then((res: any) => {
+                this.props.setUsers(res.data.items)
             })
     }
-}
-    let users = props.users
+    render() {
+        return (
+            <>
+                <div className={s.wrapper}>
+                    {this.props.users.map((user) => {
+                        return (
+                            <div key={user.id} className={s.userContainer}>
+                                <div className={s.iconContainer}>
+                                    <div>
+                                        {user.photos.small ? <img src={user.photos.small}/> : <img src={userPhoto}></img>}
+                                    </div>
+                                    {user.followed
+                                        ? <button onClick={() => this.props.unfollow(user.id)}>Unfollow</button>
+                                        : <button onClick={() => this.props.follow(user.id)}>Follow</button>}
+                                </div>
+                                <div className={s.Content}>
+                                    <div className={s.statusContainer}>
+                                        <h2>{user.name}</h2>
+                                        {/*<span>{user.location.country},<br/>{user.location.city}</span>*/}
+                                    </div>
+                                    {/*<div className={s.locationContainer}>*/}
+                                    <div className={s.status}>{user.status}</div>
+                                </div>
+                                {/*</div>*/}
+                            </div>
+                        )
+                    })
+                    }
+                    <div className={s.button}>
+                        <button>Show more</button>
+                    </div>
+                </div>
+            </>
 
-    return (
-        <div className={s.wrapper}>
-            {users.map((user) => {
-                return (
-                        <div key={user.id} className={s.userContainer}>
-                            <div className={s.iconContainer}>
-                                <div>
-                                    {user.photos.small?<img src={user.photos.small}/>:<img src={userPhoto}></img>}
-                                </div>
-                                {user.followed
-                                    ? <button onClick={()=>props.unfollow(user.id)}>Unfollow</button>
-                                    :<button onClick={()=>props.follow(user.id)}>Follow</button>}
-                            </div>
-                            <div className={s.Content}>
-                            <div className={s.statusContainer}>
-                                <h2>{user.name}</h2>
-                                    {/*<span>{user.location.country},<br/>{user.location.city}</span>*/}
-                            </div>
-                                {/*<div className={s.locationContainer}>*/}
-                                <div className={s.status}>{user.status}</div>
-                                </div>
-                            {/*</div>*/}
-                        </div>
-                )
-            })
-            }
-            <div className={s.button}>
-                <button>Show more</button>
-            </div>
-        </div>
-    );
+        );
+    }
+
 };
 
